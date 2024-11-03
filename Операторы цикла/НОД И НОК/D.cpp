29d685fe4a3431120c493cdcfb9cc5f52c7977e1
Alex <volwinu@gmail.com>
2024-11-03 18:37:37 +0300

    Update D.cpp

--- Операторы цикла/НОД И НОК/D.cpp
+++ Операторы цикла/НОД И НОК/D.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-
-
 unsigned int fib_mod(unsigned long long n) {
-
     unsigned int f0 = 0;
     unsigned int f1 = 1;
     unsigned int ans;
-    
     for (unsigned long long i = 2; i <= n; ++i) {
         ans = (f0 + f1) % 1000000000;
         f0 = f1;
         f1 = ans;
     }
-    
     return f1;
 }
-
 
 unsigned int gcd(unsigned int a, unsigned int b) {
     while (b != 0) {
@@ -29,14 +23,9 @@ unsigned int gcd(unsigned int a, unsigned int b) {
 int main() {
     unsigned long long i, j;
     std::cin >> i >> j;
-    
-
     unsigned int fi = fib_mod(i);
     unsigned int fj = fib_mod(j);
-    
-
     unsigned int answer = gcd(fi, fj);
-    
     std::cout << answer;
     return 0;
 }
